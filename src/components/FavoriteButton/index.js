2faaed48e98679e60b9deb22622b7ab40fec531f
Adam Doyle <adamldoyle@gmail.com>
2020-10-18 21:07:23 -0500

    Add tooltip to favorite star

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import FavoritesContext from '../../context/FavoritesContext';
@@ -14,8 +15,10 @@ export default function FavoriteButton({ chipName }) {
   };
 
   return (
-    <IconButton onClick={changeFavorite} aria-label="change favorite status">
-      {favorited ? <StarIcon /> : <StarBorderIcon />}
-    </IconButton>
+    <Tooltip title="change favorite status" aria-label="change favorite status">
+      <IconButton onClick={changeFavorite} aria-label="change favorite status">
+        {favorited ? <StarIcon /> : <StarBorderIcon />}
+      </IconButton>
+    </Tooltip>
   );
 }
