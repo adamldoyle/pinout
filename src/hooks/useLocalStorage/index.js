d77e1387de0723cc7d73fadc0a7ab497fb0e1993
Adam Doyle <adamldoyle@gmail.com>
2020-10-19 10:01:16 -0500

    Adapt local storage hook to support function for initialValue

--- src/hooks/useLocalStorage/index.js
+++ src/hooks/useLocalStorage/index.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
+function buildInitialValue(initialValue) {
+  if (typeof initialValue === 'function') {
+    return initialValue();
+  }
+  return initialValue;
+}
+
 export default function useLocalStorage(key, initialValue) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
@@ -8,11 +15,11 @@ export default function useLocalStorage(key, initialValue) {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      return item ? JSON.parse(item) : buildInitialValue(initialValue);
     } catch (error) {
       // If error also return initialValue
       console.log(error);
-      return initialValue;
+      return buildInitialValue(initialValue);
     }
   });
 
